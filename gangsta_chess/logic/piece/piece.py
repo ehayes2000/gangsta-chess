05fb1eb2777f72224b0c24cfc71ca6bcd7e34507
Andrew <andr3w.darling@gmail.com>
2020-08-17 14:32:49 -0600

    Add piece comments

--- gangsta_chess/logic/piece/piece.py
+++ gangsta_chess/logic/piece/piece.py
@@ -29,6 +29,7 @@ class Piece:
             return -1
         return np.where(self.board.board == self)
 
+    # Check position is on chess board
     def _in_range(self, move):
         if move[0] < 0 or move[1] < 0:
             return False
@@ -40,21 +41,31 @@ class Piece:
     def _find_valid_moves(self, steps, distance):
         self.valid_moves = set([])
         for step in steps:
+            # create array with distance number of step values
             moves_in_dir = np.array([step for i in range(distance)])
+            # cumulative sum the step values to create absolute possible moves
             moves_in_dir = np.cumsum(moves_in_dir, axis=0)
+            # center the array around the piece's current location
             moves_in_dir = np.array(self.pos()).flatten() + moves_in_dir
             for i in range(len(moves_in_dir)):
+                # Break out if current location is off the board
                 if not self._in_range(moves_in_dir[i]):
                     moves_in_dir = moves_in_dir[:i]
                     break
+                # Continue if board location is not occupied
                 if not self.board.board[tuple(moves_in_dir[i])]:
                     continue
+                # Break out after adding current move due to collision with enemy
                 elif self.board.board[tuple(moves_in_dir[i])].team != self.team:
                     moves_in_dir = moves_in_dir[:i+1]
                     break
-                else:
+                # Break out after adding current move due to collision with teammate
+                elif self.board.board[tuple(moves_in_dir[i])].team == self.team:
                     moves_in_dir = moves_in_dir[:i]
                     break
+                else:
+                    raise RuntimeError("Error! Possible move could not be sorted")
+
             for valid_pos in moves_in_dir:
                 self.valid_moves.add(tuple(valid_pos))
 
