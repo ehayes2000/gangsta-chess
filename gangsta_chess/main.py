f8205157d7194e3456a36794e56ee75da09f0a82
Andrew <andr3w.darling@gmail.com>
2020-08-18 03:14:01 -0600

    Improve piece creation process

--- gangsta_chess/main.py
+++ gangsta_chess/main.py
@@ -2,6 +2,5 @@ import gangsta_chess.ui.ui as ui
 import tkinter as tk
 
 ui.initialize()
-ui.get_board().asd = ui.get_piece("./resources/pieces/b_rook.png")
-ui.get_board().create_image(0, 0, image=ui.get_board().asd, anchor=tk.NW)
+ui.get_board().create_image(ui.get_square_cord(1, 0), image=ui.get_board().b_knight, anchor=tk.NW)
 ui.display()
